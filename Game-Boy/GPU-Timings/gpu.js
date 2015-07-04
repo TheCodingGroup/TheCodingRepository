4368812cd0810a121084a6bcf17c4a563baba79a
TheCodingGroup <ribbon4me@gmail.com>
2015-07-03 19:19:21 -0500

    Update gpu.js

--- Game-Boy/GPU-Timings/gpu.js
+++ Game-Boy/GPU-Timings/gpu.js
@@ -144,3 +144,55 @@ GPU = {
 	        ((GPU._vram[addr+1] & sx) ? 2 : 0);
 	}
     }
+
+    renderscan: function()
+    {
+	// VRAM offset for the tile map
+	var mapoffs = GPU._bgmap ? 0x1C00 : 0x1800;
+
+	// Which line of tiles to use in the map
+	mapoffs += ((GPU._line + GPU._scy) & 255) >> 3;
+	
+	// Which tile to start with in the map line
+	var lineoffs = (GPU._scx >> 3);
+
+	// Which line of pixels to use in the tiles
+	var y = (GPU._line + GPU._scy) & 7;
+
+	// Where in the tileline to start
+	var x = GPU._scx & 7;
+
+        // Where to render on the canvas
+	var canvasoffs = GPU._line * 160 * 4;
+
+	// Read tile index from the background map
+	var colour;
+	var tile = GPU._vram[mapoffs + lineoffs];
+
+	// If the tile data set in use is #1, the
+	// indices are signed; calculate a real tile offset
+	if(GPU._bgtile == 1 && tile < 128) tile += 256;
+
+	for(var i = 0; i < 160; i++)
+	{
+	    // Re-map the tile pixel through the palette
+	    colour = GPU._pal[GPU._tileset[tile][y][x]];
+
+	    // Plot the pixel to canvas
+	    GPU._scrn.data[canvasoffs+0] = colour[0];
+	    GPU._scrn.data[canvasoffs+1] = colour[1];
+	    GPU._scrn.data[canvasoffs+2] = colour[2];
+	    GPU._scrn.data[canvasoffs+3] = colour[3];
+	    canvasoffs += 4;
+
+	    // When this tile ends, read another
+	    x++;
+	    if(x == 8)
+	    {
+		x = 0;
+		lineoffs = (lineoffs + 1) & 31;
+		tile = GPU._vram[mapoffs + lineoffs];
+		if(GPU._bgtile == 1 && tile < 128) tile += 256;
+	    }
+	}
+    }
