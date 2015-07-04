7d5d83755b86b7ec66e1d0b4f24c076d3528c585
TheCodingGroup <ribbon4me@gmail.com>
2015-07-03 19:34:18 -0500

    Update key.js

--- Game-Boy/Input/key.js
+++ Game-Boy/Input/key.js
@@ -36,3 +36,32 @@ KEY = {
 
 window.onkeydown = KEY.kdown;
 window.onkeyup = KEY.kup;
+    kdown: function(e)
+    {
+    	switch(e.keyCode)
+	{
+            case 39: KEY._keys[1] &= 0xE; break;
+            case 37: KEY._keys[1] &= 0xD; break;
+            case 38: KEY._keys[1] &= 0xB; break;
+            case 40: KEY._keys[1] &= 0x7; break;
+            case 90: KEY._keys[0] &= 0xE; break;
+            case 88: KEY._keys[0] &= 0xD; break;
+            case 32: KEY._keys[0] &= 0xB; break;
+            case 13: KEY._keys[0] &= 0x7; break;
+	}
+    },
+
+    kup: function(e)
+    {
+    	switch(e.keyCode)
+	{
+            case 39: KEY._keys[1] |= 0x1; break;
+            case 37: KEY._keys[1] |= 0x2; break;
+            case 38: KEY._keys[1] |= 0x4; break;
+            case 40: KEY._keys[1] |= 0x8; break;
+            case 90: KEY._keys[0] |= 0x1; break;
+            case 88: KEY._keys[0] |= 0x2; break;
+            case 32: KEY._keys[0] |= 0x4; break;
+            case 13: KEY._keys[0] |= 0x8; break;
+	}
+    }
