8266a6988e9c1273f4d9e12b35ae38f2e5204ec7
TheCodingGroup <ribbon4me@gmail.com>
2015-07-03 19:38:47 -0500

    Update gpu.js

--- Game-Boy/GPU-Timings/gpu.js
+++ Game-Boy/GPU-Timings/gpu.js
@@ -304,3 +304,198 @@ GPU = {
 	    }
 	}
     }
+    _pal: {
+        bg: [],
+	obj0: [],
+	obj1: []
+    },
+
+    wb: function(addr)
+    {
+        switch(addr)
+	{
+	    // ...
+	    // Background palette
+	    case 0xFF47:
+	        for(var i = 0; i < 4; i++)
+		{
+		    switch((val >> (i * 2)) & 3)
+		    {
+		        case 0: GPU._pal.bg[i] = [255,255,255,255]; break;
+			case 1: GPU._pal.bg[i] = [192,192,192,255]; break;
+			case 2: GPU._pal.bg[i] = [ 96, 96, 96,255]; break;
+			case 3: GPU._pal.bg[i] = [  0,  0,  0,255]; break;
+		    }
+		}
+		break;
+
+	    // Object palettes
+	    case 0xFF48:
+	        for(var i = 0; i < 4; i++)
+		{
+		    switch((val >> (i * 2)) & 3)
+		    {
+		        case 0: GPU._pal.obj0[i] = [255,255,255,255]; break;
+			case 1: GPU._pal.obj0[i] = [192,192,192,255]; break;
+			case 2: GPU._pal.obj0[i] = [ 96, 96, 96,255]; break;
+			case 3: GPU._pal.obj0[i] = [  0,  0,  0,255]; break;
+		    }
+		}
+		break;
+
+	    case 0xFF49:
+	        for(var i = 0; i < 4; i++)
+		{
+		    switch((val >> (i * 2)) & 3)
+		    {
+		        case 0: GPU._pal.obj1[i] = [255,255,255,255]; break;
+			case 1: GPU._pal.obj1[i] = [192,192,192,255]; break;
+			case 2: GPU._pal.obj1[i] = [ 96, 96, 96,255]; break;
+			case 3: GPU._pal.obj1[i] = [  0,  0,  0,255]; break;
+		    }
+		}
+		break;
+	}
+    }
+    renderscan: function()
+    {
+        // Scanline data, for use by sprite renderer
+	var scanrow = [];
+
+        // Render background if it's switched on
+        if(GPU._switchbg)
+	{
+	    var mapoffs = GPU._bgmap ? 0x1C00 : 0x1800;
+	    mapoffs += ((GPU._line + GPU._scy) & 255) >> 3;
+	    var lineoffs = (GPU._scx >> 3);
+	    var y = (GPU._line + GPU._scy) & 7;
+	    var x = GPU._scx & 7;
+	    var canvasoffs = GPU._line * 160 * 4;
+	    var colour;
+	    var tile = GPU._vram[mapoffs + lineoffs];
+
+	    // If the tile data set in use is #1, the
+	    // indices are signed; calculate a real tile offset
+	    if(GPU._bgtile == 1 && tile < 128) tile += 256;
+
+	    for(var i = 0; i < 160; i++)
+	    {
+	        // Re-map the tile pixel through the palette
+	        colour = GPU._pal.bg[GPU._tileset[tile][y][x]];
+
+	        // Plot the pixel to canvas
+	        GPU._scrn.data[canvasoffs+0] = colour[0];
+	        GPU._scrn.data[canvasoffs+1] = colour[1];
+	        GPU._scrn.data[canvasoffs+2] = colour[2];
+	        GPU._scrn.data[canvasoffs+3] = colour[3];
+	        canvasoffs += 4;
+
+		// Store the pixel for later checking
+		scanrow[i] = GPU._tileset[tile][y][x];
+
+	        // When this tile ends, read another
+	        x++;
+	        if(x == 8)
+	        {
+	    	    x = 0;
+	    	    lineoffs = (lineoffs + 1) & 31;
+	    	    tile = GPU._vram[mapoffs + lineoffs];
+	    	    if(GPU._bgtile == 1 && tile < 128) tile += 256;
+	        }
+	    }
+	}
+
+	// Render sprites if they're switched on
+	if(GPU._switchobj)
+	{
+	    for(var i = 0; i < 40; i++)
+	    {
+	        var obj = GPU._objdata[i];
+
+		// Check if this sprite falls on this scanline
+		if(obj.y <= GPU._line && (obj.y + 8) > GPU._line)
+		{
+		    // Palette to use for this sprite
+		    var pal = obj.pal ? GPU._pal.obj1 : GPU._pal.obj0;
+
+        	    // Where to render on the canvas
+		    var canvasoffs = (GPU._line * 160 + obj.x) * 4;
+
+		    // Data for this line of the sprite
+		    var tilerow;
+
+		    // If the sprite is Y-flipped,
+		    // use the opposite side of the tile
+		    if(obj.yflip)
+		    {
+		        tilerow = GPU._tileset[obj.tile]
+			                      [7 - (GPU._line - obj.y)];
+		    }
+		    else
+		    {
+		        tilerow = GPU._tileset[obj.tile]
+			                      [GPU._line - obj.y];
+		    }
+
+		    var colour;
+		    var x;
+
+		    for(var x = 0; x < 8; x++)
+		    {
+		        // If this pixel is still on-screen, AND
+			// if it's not colour 0 (transparent), AND
+			// if this sprite has priority OR shows under the bg
+			// then render the pixel
+			if((obj.x + x) >= 0 && (obj.x + x) < 160 &&
+			   tilerow[x] &&
+			   (obj.prio || !scanrow[obj.x + x]))
+			{
+		            // If the sprite is X-flipped,
+			    // write pixels in reverse order
+			    colour = pal[tilerow[obj.xflip ? (7-x) : x]];
+
+			    GPU._scrn.data[canvasoffs+0] = colour[0];
+			    GPU._scrn.data[canvasoffs+1] = colour[1];
+			    GPU._scrn.data[canvasoffs+2] = colour[2];
+			    GPU._scrn.data[canvasoffs+3] = colour[3];
+
+			    canvasoffs += 4;
+			}
+		    }
+		}
+	    }
+	}
+    },
+    
+    rb: function(addr)
+    {
+        switch(addr)
+	{
+	    // LCD Control
+	    case 0xFF40:
+	        return (GPU._switchbg  ? 0x01 : 0x00) |
+		       (GPU._switchobj ? 0x02 : 0x00) |
+		       (GPU._bgmap     ? 0x08 : 0x00) |
+		       (GPU._bgtile    ? 0x10 : 0x00) |
+		       (GPU._switchlcd ? 0x80 : 0x00);
+
+	    // ...
+	}
+    },
+
+    wb: function(addr, val)
+    {
+        switch(addr)
+	{
+	    // LCD Control
+	    case 0xFF40:
+	        GPU._switchbg  = (val & 0x01) ? 1 : 0;
+		GPU._switchobj = (val & 0x02) ? 1 : 0;
+		GPU._bgmap     = (val & 0x08) ? 1 : 0;
+		GPU._bgtile    = (val & 0x10) ? 1 : 0;
+		GPU._switchlcd = (val & 0x80) ? 1 : 0;
+		break;
+
+	    // ...
+	}
+    }
